2975c89badda7fbf2bebc786a2719bdd3a797f81
jmlippincott <joshua.m.lippincott@gmail.com>
2021-08-21 13:30:59 -0400

    minor adjustments to exercise 11

--- src/11_check_primality_functions.py
+++ src/11_check_primality_functions.py
@@ -1,6 +1,9 @@
 # Ask the user for a number and determine whether the number is prime or not. (For those who have forgotten, a prime number is a number that has no divisors.). You can (and should!) use your answer to Exercise 4 to help you.
 
 from os import system
+import random
+
+insults = ["Seriously? Do you know what a number is?", "Number...NUMBER....N  U  M  B  E  R.", "No...those are letters.", "Were you dropped on your head as a child?", "Try the buttons at the top of your keyboard.", "I hope you've never voted.", "I bet you pay other people to pick up your food...don't you?", "Have you ever wondered why nobody has ever complimented your intelligence?"]
 
 def check_primality(num):
     factors = [a for a in range(2, num) if num % a == 0]
@@ -18,7 +21,8 @@ while True:
             check_primality(number)
             break
         else:
+            insult = random.choice(insults)
             system("clear")
-            number = input("Nope, that's not a number.  Try again: ")
+            number = input(f"{insult}. Try again: ")
 
     input("Press Enter to start over.")
